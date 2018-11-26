beb40460383ce5f9190a2fb72869a5de693d5f14
Lauren Days <lmchambers0420@gmail.com>
2018-11-26 22:59:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}"
-    
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.first?
 end
